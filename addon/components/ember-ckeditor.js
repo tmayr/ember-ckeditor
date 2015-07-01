0d0f0ed58146d4c1065bf5de48c4638bec816590
Tomas Mayr <tmayr@tmayr.com>
2015-07-01 19:28:22 -0300

    implemented a bit of bind code from https://github.com/PatentNavigation/ember-ckeditor/blob/4ae624b8742b61fd5f68ed279f0b9c1f05b09b0a/addon/components/ember-ckeditor.js

--- addon/components/ember-ckeditor.js
+++ addon/components/ember-ckeditor.js
@@ -4,18 +4,44 @@ import layout from '../templates/components/ember-ckeditor';
 
 export default Ember.Component.extend({
   layout: layout,
+  'on-change': null,
 
   config: null,
   _editor: null,
-  'on-change': null,
+  value: null,
+
+  _handleValueChange: Ember.observer('value', function () {
+    if (this._editorChanging) {
+      return;
+    }
+
+    if (this._editor) {
+      this._editor.setData(this.get('value'));
+    }
+  }),
+
+  _handleChange: function (event) {
+    this._editorChanging = true;
+    try {
+      var newValue = event.editor.getData();
+      this.set('value', newValue);
+
+      this.sendAction('on-change', {
+        event: event,
+        data: event.editor.getData()
+      });
+    } catch (e) {
+      Ember.logger.error("Error when handling scrivener change", e);
+    } finally {
+      this._editorChanging = false;
+    }
+  },
 
   didInsertElement() {
     let textarea = this.element.querySelector('.editor');
     let config = this.get('config');
     let editor = this._editor = CKEDITOR.replace(textarea, config);
-    editor.on('change', (e) => {
-      this.sendAction('on-change', e.editor.getData());
-    });
+    this._editor.on('change', this._handleChange.bind(this));
   },
 
   willDestroyElement() {
